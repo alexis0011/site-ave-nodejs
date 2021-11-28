5429fa84ba6dc93f40cb5ffb184c8725196a0855
Alexx <kvartirant1998@gmail.com>
2021-11-28 07:37:49 +0300

    delete doctype

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import path from 'path'
 
 const __dirname = path.resolve()
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3003
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, 'static')))
